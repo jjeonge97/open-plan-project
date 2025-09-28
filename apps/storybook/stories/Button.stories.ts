edf541cf0e0c73e5525a33f8ee0cb9f4559e1f62
hyeonjeong <cowguswjd@gmail.com>
2025-09-28 15:50:58 +0900

    feat:메인페이지 생성

--- apps/storybook/stories/Button.stories.ts
+++ apps/storybook/stories/Button.stories.ts
@@ -2,7 +2,8 @@ import type { Meta, StoryObj } from "@storybook/react-vite";
 
 import { fn } from "storybook/test";
 
-import { Button } from "./Button";
+import { Button } from "@open-plan/ui";
+import "@open-plan/ui/button.css";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
---
+++ apps/web/src/pages/Home.tsx
@@ -0,0 +1,58 @@
+import styled from "styled-components";
+import { Button } from "@open-plan/ui";
+import "@open-plan/ui/button.css";
+
+const Home = () => {
+  return (
+    <Container>
+      <Header>지원지분 성함을 적어주세요</Header>
+      <Content>
+        <Text>
+          안녕하세요
+          <br />
+          김현정입니다
+        </Text>
+      </Content>
+      <ButtonWrapper>
+        <Button label="다음" />
+      </ButtonWrapper>
+    </Container>
+  );
+};
+
+export default Home;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const Header = styled.div`
+  font-size: 15px;
+  color: #1a1a1a;
+  padding: 17px 20px;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.span`
+  font-size: 28px;
+  font-weight: 500;
+  text-align: center;
+  color: #111;
+`;
+
+const ButtonWrapper = styled.div`
+  padding: 40px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
